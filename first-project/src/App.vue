3ccfd46a46f7acd83df6229ec9d07518cd561385
sashkaool <sasha.dubilo@gmail.com>
2021-06-28 18:34:46 +0300

    calc

--- first-project/src/App.vue
+++ first-project/src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <Calc />
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Calc from './components/Calc.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Calc
   }
 }
 </script>
---
+++ first-project/src/components/Calc.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+        <input type="number" v-model.number="oper1"/>
+        <input type="number" v-model.number="oper2"/>
+        = {{ result }}
+        <p>
+            <button v-on:click="sum()">+</button>
+            <button @click="result = oper1 - oper2">-</button>
+            <button v-on:click="result = oper1 * oper2">*</button>
+            <button @click="div()">/</button>
+            <button @click="degree()">В степень</button>
+        </p>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Calc',
+    data: () => ({
+        oper1: 0,
+        oper2: 0,
+        result: 0
+    }),
+    methods: {
+        sum() {
+            this.result = this.oper1 + this.oper2;
+        },
+        div() {
+            this.result = this.oper1 / this.oper2;
+            this.result = parseInt(new String(this.result));
+        },
+        degree() {
+            this.result = 1;
+            for (let i = 0; i < this.oper2; i++) {
+                this.result *= this.oper1;
+            }
+        }
+    } 
+}
+</script>
+
+<style>
+
+</style>
